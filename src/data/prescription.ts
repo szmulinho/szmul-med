6e13012ad7dee7e80ea0472039ef2c585bf11630
szmulinho <samik1997@gmail.com>
2023-10-19 15:50:27 +0200

    data

--- src/data/prescription.ts
+++ src/data/prescription.ts
@@ -12,12 +12,12 @@ export interface Prescription {
 }
 
 export async function getPresc(): Promise<Prescription[]> {
-    const response = await axios.get('https://szmul-med-prescription.onrender.com/prescs');
+    const response = await axios.get('http://localhost:8080/prescs');
     return response.data;
 }
 
 export async function postPresc(postData: Prescription): Promise<Prescription[]> {
-    const response = await axios.post('https://szmul-med-prescription.onrender.com/presc', postData);
+    const response = await axios.post('http://localhost:8080/presc', postData);
     return response.data;
 }
 
@@ -25,18 +25,18 @@ export async function postPresc(postData: Prescription): Promise<Prescription[]>
 
 
 export async function GetPrescID(preId: string): Promise<Prescription> {
-    const response = await axios.get(`https://szmul-med-prescription.onrender.com/presc/${preId}`);
+    const response = await axios.get(`http://localhost:8080/presc/${preId}`);
     return response.data;
 }
 
 export async function GetPatientPresc(patient: string): Promise<Prescription> {
-    const response = await axios.get(`https://szmul-med-prescription.onrender.com/patient/${patient}`);
+    const response = await axios.get(`http://localhost:8080/patient/${patient}`);
     return response.data;
 }
 
 export async function deletePresc(id: string): Promise<void> {
     try {
-        await axios.delete(`https://szmul-med-prescription.onrender.com/prescs/${id}`);
+        await axios.delete(`http://localhost:8080/prescs/${id}`);
     } catch (error) {
         console.error(`Error deleting prescription with ID ${id}: ${error}`);
     }
