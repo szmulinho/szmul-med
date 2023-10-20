9619cb4605ae3f8e942c34acdbf460e317f43bef
szmulinho <samik1997@gmail.com>
2023-10-20 14:04:02 +0200

    data

--- src/data/drugstore.ts
+++ src/data/drugstore.ts
@@ -8,34 +8,34 @@ export interface Drug {
 }
 
 export async function getAllDrugs(): Promise<Drug[]> {
-    const response = await axios.get('https://3.75.158.163:8081/drugs');
+    const response = await axios.get('216.24.57.3:443/drugs');
     console.log("leki", response.data)
     return response.data;
 }
 
 export async function getDrugByName(id: string): Promise<void> {
     try {
-        await axios.get(`https://3.75.158.163:8081/drugs/${name}`);
+        await axios.get(`216.24.57.3:443216.24.57.3:443216.24.57.3:443/drugs/${name}`);
     } catch (error) {
         console.error(`Error deleting prescription with ID ${name}: ${error}`);
     }
 }
 
 export async function postDrug(postData: Drug): Promise<Drug[]> {
-    const response = await axios.post('https://3.75.158.163:8081/drug', postData);
+    const response = await axios.post('216.24.57.3:443/drug', postData);
     return response.data;
 }
 
 export async function deleteDrug(id: string): Promise<void> {
     try {
-        await axios.delete(`https://3.75.158.163:8081/drugs/${id}`);
+        await axios.delete(`216.24.57.3:443/drugs/${id}`);
     } catch (error) {
         console.error(`Error deleting prescription with ID ${id}: ${error}`);
     }
 }
 
 export async function updateDrug(id: string, updatedDrug: Partial<Drug>): Promise<Drug> {
-    const response = await axios.patch(`https://3.75.158.163:8081/drugs/${id}`, updatedDrug);
+    const response = await axios.patch(`216.24.57.3:443/drugs/${id}`, updatedDrug);
     return response.data;
 }
 
