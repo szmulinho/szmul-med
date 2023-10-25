bf04c7d7cdd22fc28ad737bfd2151391b7272384
szmulinho <samik1997@gmail.com>
2023-10-25 13:00:05 +0200

    github

--- src/pages/githubUsers/profile.tsx
+++ src/pages/githubUsers/profile.tsx
@@ -6,13 +6,6 @@ import { useNavigate } from 'react-router-dom';
 export function GithubUserProf() {
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            navigate('/login');
-        }
-    }, []); // Pusta tablica dependencies oznacza, że useEffect uruchomi się tylko raz, po zamontowaniu komponentu.
-
     return (
         <div className="d-flex">
             <GithubProfile />
