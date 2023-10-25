5481d0b3cdb526d0f93224985f96ad87147c34f8
szmulinho <samik1997@gmail.com>
2023-10-25 17:29:01 +0200

    login

--- src/compontents/Github/Login.tsx
+++ src/compontents/Github/Login.tsx
@@ -1,19 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 export function LoginWithGithub() {
     const [githubData, setGithubData] = useState<string>('');
-    const [code, setCode] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const code = localStorage.getItem('code');
+            if (code) {
+                try {
+                    const response = await fetch(`https://szmul-med-github-login.onrender.com/github/callback?code=${code}`);
+                    if (response.ok) {
+                        const result = await response.json();
+                        setGithubData(JSON.stringify(result, null, 2));
+                    } else {
+                        console.error('Failed to fetch data from GitHub API');
+                    }
+                } catch (error) {
+                    console.error('Error occurred while fetching data:', error);
+                } finally {
+                    setLoading(false);
+                }
+            } else {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     const handleLoginClick = async () => {
         try {
             const response = await fetch('https://szmul-med-github-login.onrender.com/github/login/');
             if (response.ok) {
                 const result = await response.json();
-                const code = result.code; // Załóżmy, że 'code' jest polem w odpowiedzi z GitHub OAuth
+                const code = result.code;
                 localStorage.setItem('code', code);
-                setGithubData(JSON.stringify(result, null, 2));
+                window.location.href = result.redirectURL; // Przekieruj do GitHub OAuth
             } else {
                 console.error('Failed to fetch data from GitHub API');
             }
@@ -35,13 +60,14 @@ export function LoginWithGithub() {
                     },
                 }}
                 onClick={handleLoginClick}
+                disabled={loading}
             >
                 Login with GitHub
             </Button>
-            {githubData ? (
-                <pre>{githubData}</pre>
+            {loading ? (
+                <div>Loading...</div>
             ) : (
-                <div>Loading GitHub data...</div>
+                <pre>{githubData}</pre>
             )}
         </div>
     );
