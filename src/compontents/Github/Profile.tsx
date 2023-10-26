dff6d8956d9cdea77d352b0f8f2cfcc8f5e98fc7
szmulinho <samik1997@gmail.com>
2023-10-26 12:43:04 +0200

    profile

--- src/compontents/Github/Profile.tsx
+++ src/compontents/Github/Profile.tsx
@@ -1,28 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGitHubUserContext } from '../../context/Github';
 
 export const GithubProfile: React.FC = () => {
     const { githubUser, isLoggedIn, handleCallback } = useGitHubUserContext();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const code = localStorage.getItem('githubCode');
         if (code && !isLoggedIn) {
             // Call the handleCallback function to exchange code for user data
-            handleCallback(code);
+            handleCallback(code)
+                .then(() => {
+                    setLoading(false);
+                })
+                .catch(error => {
+                    console.error('Error fetching user data:', error);
+                    setLoading(false);
+                });
+        } else {
+            setLoading(false);
         }
     }, [handleCallback, isLoggedIn]);
 
     return (
         <div>
             <h2>User Profile</h2>
-            {isLoggedIn && githubUser ? (
+            {loading ? (
+                <div>Loading user data...</div>
+            ) : isLoggedIn && githubUser ? (
                 <div>
                     <h3>{githubUser.username}</h3>
                     <h3>{githubUser.email}</h3>
                     <h3>{githubUser.role}</h3>
                 </div>
             ) : (
-                <div>Loading user data...</div>
+                <div>User not logged in or data not available.</div>
             )}
         </div>
     );
--- src/context/Github.tsx
+++ src/context/Github.tsx
@@ -13,24 +13,25 @@ export interface GitHubUserContextProps {
     isLoggedIn: boolean;
     login: () => void;
     logout: () => void;
-    handleCallback: (code: string) => void;
+    handleCallback: (code: string) => Promise<void> // Return a Promise
     setGithubUser: Dispatch<SetStateAction<GithubUser | undefined>>;
 }
 
 const defaultContext: GitHubUserContextProps = {
     isLoggedIn: false,
-    login: () => {},
-    logout: () => {},
-    handleCallback: (code: string) => {},
-    setGithubUser: () => {},
+    login: async () => {}, // Return a Promise<void>
+    logout: async () => {}, // Return a Promise<void>
+    handleCallback: async (code: string) => {}, // Return a Promise<void>
+    setGithubUser: () => {}, // You can use setState, so it doesn't return anything
 };
+
 
 const githubClientId = '065d047663d40d183c04';
 const redirectUri = 'https://szmul-med.onrender.com/github_user';
 
 export const GitHubUserContext = createContext<GitHubUserContextProps>(defaultContext);
 
-export const GitHubUserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+const GitHubUserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [user, setUser] = useState<GithubUser | undefined>(undefined);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -47,7 +48,7 @@ export const GitHubUserProvider: React.FC<{ children: ReactNode }> = ({ children
         setUser(userData);
     };
 
-    const handleCallback = async (code: string) => {
+    const handleCallback: GitHubUserContextProps['handleCallback'] = async (code: string): Promise<void> => {
         try {
             const response = await axios.get(`/github/callback?code=${code}`);
             const userData: GithubUser = response.data;
@@ -55,8 +56,13 @@ export const GitHubUserProvider: React.FC<{ children: ReactNode }> = ({ children
             setIsLoggedIn(true);
         } catch (error) {
             console.error('Error occurred while fetching data:', error);
+            throw error; // Re-throw the error to be caught by the caller
         }
     };
+
+
+
+
 
     const contextValues: GitHubUserContextProps = {
         isLoggedIn,
