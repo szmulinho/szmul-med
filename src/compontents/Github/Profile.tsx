3a9d5badf6c4892b56318ca2fa070798c3f22be9
szmulinho <samik1997@gmail.com>
2023-11-01 19:59:26 +0100

    profile

--- src/compontents/Github/Profile.tsx
+++ src/compontents/Github/Profile.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect } from 'react';
-import { GitHubUserContext, GitHubUserContextProps } from '../../context/Github';
+import React, {useContext, useEffect} from 'react';
+import {GitHubUserContext, GitHubUserContextProps} from '../../context/Github';
 
 export const GithubProfile: React.FC = () => {
     const { githubUser, isLoggedIn, handleCallback, handleLogout } = useContext(GitHubUserContext) as GitHubUserContextProps;
@@ -8,15 +8,13 @@ export const GithubProfile: React.FC = () => {
         const fetchData = async () => {
             if (isLoggedIn) {
                 try {
-                    // Pobierz kod autoryzacji z adresu URL
-                    const urlParams = new URLSearchParams(window.location.search);
-                    const code = urlParams.get('code');
-
+                    // Retrieve code from localStorage
+                    const code = localStorage.getItem('code');
                     if (code) {
-                        // Wywołaj funkcję handleCallback z kodem autoryzacji
+                        // Call the handleCallback function with the code parameter
                         await handleCallback(code);
                     } else {
-                        console.error('Code not found in URL.');
+                        console.error('Code not found in localStorage.');
                     }
                 } catch (error) {
                     console.error('Error occurred while fetching user data:', error);
@@ -24,19 +22,20 @@ export const GithubProfile: React.FC = () => {
             }
         };
 
-        fetchData(); // Wywołaj funkcję fetchData po załadowaniu komponentu
+        fetchData(); // Call the fetchData function when component mounts
     }, [handleCallback, isLoggedIn]);
+
 
     return (
         <div>
             <h2>User Profile</h2>
             {isLoggedIn && githubUser ? (
                 <div>
-                    <img src={githubUser.avatar_url} alt="User Avatar" />
-                    <h3>{githubUser.role}</h3>
-                    <h3>{githubUser.email}</h3>
-                    <h3>{githubUser.login}</h3>
-                    <button onClick={handleLogout}>Logout</button>
+                    <img src={githubUser.avatar_url}/>
+                        <h3>{githubUser.role}</h3>
+                        <h3>{githubUser.email}</h3>
+                        <h3>{githubUser.login}</h3>
+                       <button onClick={handleLogout}>Logout</button>
                 </div>
             ) : (
                 <div>Loading user data...</div>
