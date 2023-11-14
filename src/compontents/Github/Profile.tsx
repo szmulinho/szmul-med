7cdf7b864809e04fda7c8da46161d122ce58804c
szmulinho <samik1997@gmail.com>
2023-11-14 19:35:29 +0100

    profile

--- src/compontents/Github/Profile.tsx
+++ src/compontents/Github/Profile.tsx
@@ -23,7 +23,7 @@ export const GithubProfile: React.FC = () => {
             }
         };
 
-        fetchData(); // Call the fetchData function when component mounts
+        fetchData();
     }, [GetGithubUserData, githubUser]);
 
 
@@ -34,9 +34,9 @@ export const GithubProfile: React.FC = () => {
                 <div>
                     <img src={githubUser.avatar_url}/>
                         <h3>{githubUser.email}</h3>
-                    <h3>Hello{githubUser.login}!</h3>
-                    <h3>Followers:{githubUser.followers}</h3>
-                    <h3>GithubUser Role:{githubUser.role}</h3>
+                    <h1>Hello {githubUser.login}!</h1>
+                    <h2>Followers: {githubUser.followers}</h2>
+                    <h3>GithubUser Role: {githubUser.role}</h3>
                     <button onClick={handleLogout}>Logout</button>
                 </div>
             ) : (
