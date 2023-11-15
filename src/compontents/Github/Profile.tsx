4ed6dd1714b3144d8f7571673ce279c5a644a0b2
szmulinho <samik1997@gmail.com>
2023-11-15 21:58:18 +0100

    data

--- src/compontents/Github/Profile.tsx
+++ src/compontents/Github/Profile.tsx
@@ -1,15 +1,17 @@
-import React, {useContext, useEffect} from 'react';
-import {GitHubUserContext, GitHubUserContextProps} from '../../context/Github';
-import {GetGithubUserData} from "../../data/github";
+import React, { useContext, useEffect, useState } from 'react';
+import { GitHubUserContext, GitHubUserContextProps } from '../../context/Github';
+import { GetGithubUserData, GithubUser } from '../../data/github';
 
-export const GithubProfile: React.FC = () => {
+const Profile: React.FC = () => {
     const { githubUser, handleLogout } = useContext(GitHubUserContext) as GitHubUserContextProps;
+    const [userData, setUserData] = useState<GithubUser | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
             if (githubUser) {
                 try {
-                    await GetGithubUserData(githubUser);
+                    const { user, token } = await GetGithubUserData(githubUser);
+                    setUserData(user);
                 } catch (error) {
                     console.error('Error occurred while fetching user data:', error);
                 }
@@ -17,20 +19,19 @@ export const GithubProfile: React.FC = () => {
         };
 
         fetchData();
-    }, [GetGithubUserData, githubUser]);
-
+    }, [githubUser]);
 
     return (
         <div>
             <h2>User Profile</h2>
-            {githubUser ? (
+            {userData ? (
                 <div>
-                    <img src={githubUser.avatar_url}/>
-                        <h3>{githubUser.email}</h3>
-                    <h1>Hello {githubUser.login}!</h1>
-                    <h2>Followers: {githubUser.followers}</h2>
-                    <h3>GithubUser Role: {githubUser.role}</h3>
-                    <h3>HTML Url: {githubUser.html_url}</h3>
+                    <img src={userData.avatar_url} alt="User Avatar" />
+                    <h3>{userData.email}</h3>
+                    <h1>Hello {userData.login}!</h1>
+                    <h2>Followers: {userData.followers}</h2>
+                    <h3>GithubUser Role: {userData.role}</h3>
+                    <h3>HTML Url: {userData.html_url}</h3>
                     <button onClick={handleLogout}>Logout</button>
                 </div>
             ) : (
@@ -40,4 +41,4 @@ export const GithubProfile: React.FC = () => {
     );
 };
 
-export default GithubProfile;
+export default Profile;
--- src/data/github.ts
+++ src/data/github.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosRequestConfig } from "axios";
-import {User} from "./users";
 
 export interface GithubUser {
     id: number;
@@ -13,12 +12,18 @@ export interface GithubUser {
     followers: number;
 }
 
-
 export async function handleCallback(code?: string | null): Promise<GithubUser | null> {
     const headers = code ? { Authorization: `Bearer ${code}` } : {};
     const response = await axios.get('https://szmul-med-github-login.onrender.com/github/callback', { headers });
+
     if (response.status === 200) {
-        return response.data;
+        const githubUser: GithubUser = response.data;
+
+        // Pass the obtained token to the next function
+        const token = githubUser.token;
+        const userData = await GetGithubUserData(githubUser);
+
+        return userData.user;
     } else {
         return null;
     }
