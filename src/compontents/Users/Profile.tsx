b9f12cfef53505638d7e6e3b6555c4f8b735a103
szmulinho <samik1997@gmail.com>
2023-11-30 22:06:49 +0100

    user profile

--- src/compontents/Users/Profile.tsx
+++ src/compontents/Users/Profile.tsx
@@ -4,17 +4,19 @@ import { UserContext, UserContextProps } from "../../context/UserContext";
 import { GetPatientPresc, Prescription } from "../../data/prescription"; // Assume API functions are imported from the correct path
 import { Modal, Button } from 'react-bootstrap';
 import {GetOrderByName, Order} from "../../data/orders";
+import {GitHubUserContext, GitHubUserContextProps} from "../../context/Github";
 
 export function CuProfile() {
     const navigate = useNavigate();
     const { user, setUser, logout } = useContext(UserContext) as UserContextProps;
+    const { githubUser, setGithubUser } = useContext(GitHubUserContext) as GitHubUserContextProps;
     const [prescription, setPrescription] = useState<Prescription | null>(null);
     const [order, setOrder] = useState<Order | null>(null);
     const [showResult, setShowResult] = useState(false);
     const [showResult2, setShowResult2] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    if (user === null || user === undefined) {
+    if (user === null || user === undefined || githubUser === null || githubUser === undefined) {
         navigate('/login');
         return null;
     }
@@ -23,14 +25,14 @@ export function CuProfile() {
         try {
             setLoading(true);
             const token = localStorage.getItem('token');
-            if (!token || !user) {
+            if (!token || !user || !githubUser) {
                 console.error("Token or user not available");
                 navigate('/login');
                 return;
             }
 
-            console.log("Fetching prescription for user:", user.login);
-            const fetchedPrescription = await GetPatientPresc(user.login);
+            console.log("Fetching prescription for user:", user.login || githubUser.login);
+            const fetchedPrescription = await GetPatientPresc(user.login || githubUser.login);
             console.log("Fetched prescription data:", fetchedPrescription);
 
             setPrescription(fetchedPrescription);
@@ -53,7 +55,7 @@ export function CuProfile() {
             }
 
             console.log("Fetching orders for user:", user.login);
-            const fetchedOrder = await GetOrderByName(user.login);
+            const fetchedOrder = await GetOrderByName(user.login || githubUser.login);
             console.log("Fetched orders data:", fetchedOrder);
 
             setOrder(fetchedOrder);
@@ -84,7 +86,7 @@ export function CuProfile() {
     return (
         <div>
             <h2>User Profile</h2>
-            <p>Welcome, {user.login}!</p>
+            <p>Welcome, {user.login || githubUser?.login}!</p>
 
             <Button variant="secondary" onClick={handleShowPrescriptions}>Show my prescriptions</Button>
 
@@ -100,7 +102,7 @@ export function CuProfile() {
                             <pre>{JSON.stringify(prescription, null, 2)}</pre>
                         </div>
                     ) : (
-                        <p>No prescription found for {user.login}.</p>
+                        <p>No prescription found for {user.login || githubUser?.login}.</p>
                     )}
                 </Modal.Body>
 
@@ -126,7 +128,7 @@ export function CuProfile() {
                             <pre>{JSON.stringify(order, null, 2)}</pre>
                         </div>
                     ) : (
-                        <p>No orders found for {user.login}.</p>
+                        <p>No orders found for {user.login || githubUser?.login}.</p>
                     )}
                 </Modal.Body>
 
